d203679730547b2d8c87d5cbef41b2c2ee68f26e
Matt Patterson <matt@reprocessed.org>
2013-01-31 19:00:07 +0100

    remove Motivations from public view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,16 +8,6 @@ class HomeController < ApplicationController
     end
   end
 
-  def motivation
-    @users = User.with_signup_reasons.shuffle
-    @losers = User.without_signup_reasons.shuffle
-  end
-
-  def my_motivation
-    authenticate_user!
-    redirect_to edit_user_path(current_user) if current_user
-  end
-
   def anonymous_proposals
   end
 end
