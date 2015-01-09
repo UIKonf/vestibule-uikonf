c91e6a4ee8dce2378b06efdb1b20778e346bef24
Engin Kurutepe <engin@kurutepe.com>
2015-01-09 21:32:04 +0100

    redirect to about when inactive

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,11 @@ class HomeController < ApplicationController
     if user_signed_in?
       redirect_to dashboard_path
     else
-      redirect_to proposals_path
+          if Vestibule.mode_of_operation.mode == :inactive
+            redirect_to about_path
+          else 
+            redirect_to dashboard_path
+          end
     end
   end
 
