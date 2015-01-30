fefb7246a315a2ad94005d45ab828a0f33b9bc6a
Engin Kurutepe <engin@kurutepe.com>
2015-01-30 10:18:00 +0100

    setup action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,18 @@ Vestibule::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'sub4.mail.dreamhost.com',
+    port:                 587,
+    user_name:            ENV['MAIL_USERNAME']
+    password:             ENV['MAIL_PASSWORD']
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'questions@uikonf.com'}
+  
   # Enable threaded mode
   # config.threadsafe!
 
