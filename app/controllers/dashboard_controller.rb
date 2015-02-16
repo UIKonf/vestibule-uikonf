e6d2b0a4d9d70e7a38f62bc7a5a61a4dd077b0f0
Engin Kurutepe <engin@kurutepe.com>
2015-02-16 14:48:37 +0100

    randomize proposals

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
 
   def index
     @your_proposals = current_user.proposals
-    @proposals_you_should_look_at = current_user.proposals_you_should_look_at
+    @proposals_you_should_look_at = current_user.proposals_you_should_look_at.shuffle
     @proposals_that_have_changed = current_user.proposals_that_have_changed
     @proposals_that_have_been_withdrawn = current_user.proposals_that_have_been_withdrawn
   end
