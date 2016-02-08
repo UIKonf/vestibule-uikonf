aaf29a0daed2eb8acff2b0198d01bad5bca3b29f
Engin Kurutepe <engin@kurutepe.com>
2016-02-08 16:48:26 +0100

    make emails address smarter

--- app/mailers/suggestions_mailer.rb
+++ app/mailers/suggestions_mailer.rb
@@ -6,8 +6,15 @@ class SuggestionsMailer < ActionMailer::Base
   
   def new_suggestion(suggestion)
     @suggestion = suggestion
-
-    mail to: @suggestion.proposal.proposer.email,
+    
+    to_address = ''
+    if @suggestion.proposal.proposer.email.is_a?(Hash)
+      to_address = @suggestion.proposal.proposer.email[:email]
+    else 
+      to_address = @suggestion.proposal.proposer.email
+    end
+    
+    mail to: to_address
          subject: "Someone just posted a suggestion on '#{@suggestion.proposal.title}'!"
   end
 end
