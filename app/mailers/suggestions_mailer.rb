3209149ed7ddaebfc7b854c70ce049e37bab6d34
Engin Kurutepe <engin@kurutepe.com>
2016-02-08 21:21:01 +0100

    use Hashie:Mash functionality

--- app/mailers/suggestions_mailer.rb
+++ app/mailers/suggestions_mailer.rb
@@ -11,8 +11,8 @@ class SuggestionsMailer < ActionMailer::Base
     to_address =
       if email.is_a?(String)
         to_address = email
-      elsif email.respond_to?(:[])
-        to_address = email[:email]
+      elsif email.respond_to?(:email) && email.email? 
+        to_address = email.email
       else 
         to_address = email
       end
