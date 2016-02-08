2631f2cb3fb381cf578cee3e3f43229b163b2738
Engin Kurutepe <engin@kurutepe.com>
2016-02-08 18:31:40 +0100

    improve mailer

--- app/mailers/suggestions_mailer.rb
+++ app/mailers/suggestions_mailer.rb
@@ -7,12 +7,13 @@ class SuggestionsMailer < ActionMailer::Base
   def new_suggestion(suggestion)
     @suggestion = suggestion
     
-    to_address = ''
-    if @suggestion.proposal.proposer.email.is_a?(Hash)
-      to_address = @suggestion.proposal.proposer.email['email']
-    else 
-      to_address = @suggestion.proposal.proposer.email
-    end
+    email = @suggestion.proposal.proposer.email
+    to_address =
+      if email.respond_to?(:[])
+        to_address = email[:email]
+      else 
+        to_address = email
+      end
     
     mail to: to_address, subject: "Someone just posted a suggestion on '#{@suggestion.proposal.title}'!"
   end
