41dce43fa09ac1e630cd003d7681e96afa31fe4a
Engin Kurutepe <engin@kurutepe.com>
2016-02-10 15:36:54 +0100

    take the first returned email

--- app/mailers/suggestions_mailer.rb
+++ app/mailers/suggestions_mailer.rb
@@ -9,7 +9,7 @@ class SuggestionsMailer < ActionMailer::Base
     
     emailObject = @suggestion.proposal.proposer.email
         
-    to_address = (emailObject.include? "Hashie::Mash") ? emailObject.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i) { |x| puts x } : emailObject
+    to_address = (emailObject.include? "Hashie::Mash") ? emailObject.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i).first : emailObject
     
     mail to: to_address, subject: "Someone just posted a suggestion on '#{@suggestion.proposal.title}'!"
   end
