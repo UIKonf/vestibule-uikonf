d0e0b159a1bcb67b7edd2c71055d57351d80c9f4
Engin Kurutepe <engin@kurutepe.com>
2016-02-10 15:25:58 +0100

    a hack to take the email out of Mash description

--- app/mailers/suggestions_mailer.rb
+++ app/mailers/suggestions_mailer.rb
@@ -8,8 +8,8 @@ class SuggestionsMailer < ActionMailer::Base
     @suggestion = suggestion
     
     emailObject = @suggestion.proposal.proposer.email
-    
-    to_address = (emailObject.respond_to?(:email?) && emailObject.email?) ? emailObject.email : emailObject.to_s
+        
+    to_address = (emailObject.include? "Hashie::Mash") ? emailObject.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i) { |x| puts x } : emailObject
     
     mail to: to_address, subject: "Someone just posted a suggestion on '#{@suggestion.proposal.title}'!"
   end
